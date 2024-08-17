74b773bed8910a35416e620ca0eb3df4abee8324
Prashant Shaw <prashant.shaw926@gmail.com>
2024-08-17 17:10:08 +0530

    added ng-content

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -2,11 +2,12 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SearchComponent } from './search/search.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { FeaturedBrandsComponent } from './featured-brands/featured-brands.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [SearchComponent, ProductListComponent, ProductDetailComponent],
+  imports: [SearchComponent, ProductListComponent, ProductDetailComponent, FeaturedBrandsComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
 })
---
+++ src/app/container/featured-brands/featured-brands.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-brands',
+  standalone: true,
+  imports: [],
+  templateUrl: './featured-brands.component.html',
+  styleUrl: './featured-brands.component.css',
+})
+export class FeaturedBrandsComponent {
+  // featuredBrand = [
+  //   {
+  //     title: 'New Arrivals in Addidas',
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  //   },
+  //   {
+  //     title: 'New Arrivals in Nike',
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  //   },
+  //   {
+  //     title: 'New Arrivals in Reebok',
+  //     description:
+  //       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  //   },
+  // ];
+}
