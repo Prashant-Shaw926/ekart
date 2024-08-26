3e0161d5e282556f89e2bf61b63c9de02386d31a
Prashant Shaw <prashant.shaw926@gmail.com>
2024-08-27 00:48:46 +0530

    added appHighlight (hostListener) and renderer2

--- src/app/customDirective/SetBackground.directive.ts
+++ src/app/customDirective/SetBackground.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[setBackground]',
@@ -6,12 +6,15 @@ import { Directive, ElementRef, OnInit } from "@angular/core";
 })
 export class setBackground implements OnInit{
     // private element: ElementRef;
-    constructor(private element: ElementRef){
+    constructor(private element: ElementRef, private renderer: Renderer2){
         // this.element = element;
     }
 
     ngOnInit(){
-        this.element.nativeElement.style.backgroundColor = '#36454F';
-        this.element.nativeElement.style.color = '#fff'
+        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#36454F');
+        this.renderer.setStyle(this.element.nativeElement, 'color', '#fff');
+        // this.renderer.setAttribute(this.element.nativeElement, 'title', 'This is an example title');
+        // this.element.nativeElement.style.backgroundColor = '#36454F';
+        // this.element.nativeElement.style.color = '#fff'
     }
 }
